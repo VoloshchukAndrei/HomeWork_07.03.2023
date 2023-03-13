3bd65340d290e6e59d2b77487a28a5d63ad1cf16
Andrei Voloshchuk <troubleshooter.andrei@gmail.com>
2023-03-14 03:13:21 +0500

    треугольник Паскаля

---
+++ Task061/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.Clear();
+
+Console.Write("Введите нужное количество строк треугольника Паскаля: ");
+int rows = int.Parse(Console.ReadLine());
+PrintTriangle(rows);
+
+void PrintTriangle(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        // создаём после каждой строки n-i отступов от левой стороны консоли, чем ниже строка, тем меньше отступ
+        for (int c = 0; c <= (n - i); c++) 
+        {
+            Console.Write("  ");
+        }
+        for (int c = 0; c <= i; c++)
+        {
+            Console.Write("   "); // создаём пробелы между элементами треугольника
+            Console.Write(factorial(i) / (factorial(c) * factorial(i - c))); //формула вычисления элементов треугольника
+        }
+        Console.WriteLine(); // после каждой строки с числами отступаем две пустые строчки
+    }
+}
+//Функция для подсчета факториала
+float factorial(int n)
+{
+    float x = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        x *= i;
+    }
+    return x;
+}
