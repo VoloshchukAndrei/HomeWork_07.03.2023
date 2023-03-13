dfc813076e14716a1dc39b88c608c559ab560115
Andrei Voloshchuk <troubleshooter.andrei@gmail.com>
2023-03-14 03:14:25 +0500

    Array spiral

---
+++ Task062/Program.cs
@@ -0,0 +1,115 @@
+﻿// **Задача 62**. Заполните спирально массив 4 на 4. 
+// Например, на выходе получается вот такой массив:
+// 1 2 3 4
+// 12 13 14 5
+// 11 16 15 6
+// 10 9 8 7
+
+Console.Clear();
+
+Console.Write("Введите количество едениц в грани массива: ");
+int facet = int.Parse(Console.ReadLine());
+
+int[,] array = GetArray(facet);
+PrintArray(array);
+
+int[,] GetArray(int M)
+{
+    int[,] result = new int[M, M];
+    int i = 0;
+    int j = 0;
+    int rowE = M - 1;
+    int columnE = M - 1;
+    int rowS = 0;
+    int columnS = 0;
+    bool left = true;
+    bool top = true;
+    int count = 0;
+    while (count < M * M)
+    {
+        count++;
+        result[i, j] = count;
+        //идем вправо
+        if (left && top)
+        {
+            if (j == columnE)
+            {
+                rowS++;
+                top = true;
+                left = false;
+                i++;
+                continue;
+            }
+            else
+            {
+                j++;
+                continue;
+            }
+        }
+        //идем вниз
+        if (!left && top)
+        {
+            if (i == rowE)
+            {
+                columnE--;
+                top = false;
+                left = false;
+                j--;
+                continue;
+            }
+            else
+            {
+                i++;
+                continue;
+            }
+        }
+        //идем влево
+        if (!left && !top)
+        {
+            if (j == columnS)
+            {
+                rowE--;
+                top = false;
+                left = true;
+                i--;
+                continue;
+            }
+            else
+            {
+                j--;
+                continue;
+            }
+        }
+        //Идем вверх
+        if (left && !top)
+        {
+            if (i == rowS)
+            {
+                columnS++;
+                top = true;
+                left = true;
+                j++;
+                continue;
+            }
+            else
+            {
+                i--;
+                continue;
+            }
+        }
+
+    }
+    return result;
+}
+
+void PrintArray(int[,] N)
+{
+    for (int i = 0; i < N.GetLength(0); i++)
+    {
+        for (int j = 0; j < N.GetLength(1); j++)
+        {
+            Console.Write($"{N[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
